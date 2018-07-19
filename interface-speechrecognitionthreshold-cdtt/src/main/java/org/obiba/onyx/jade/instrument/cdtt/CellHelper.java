8a3f1284aa3b20f15cd350153349436252150de3
dean <inglis.dl@gmail.com>
2018-07-19 15:33:31 -0400

    WIP: CDTT integration

---
+++ interface-speechrecognitionthreshold-cdtt/src/main/java/org/obiba/onyx/jade/instrument/cdtt/CellHelper.java
@@ -0,0 +1,54 @@
+package org.obiba.onyx.jade.instrument.cdtt;
+
+import org.obiba.onyx.util.data.Data;
+
+public class CellHelper {
+
+  public enum Action {
+    VALIDATE,
+    WRITE,
+    READ
+  }
+
+  public CellHelper(Integer direction, Data data, Action type) {
+    this.direction = direction;
+    this.data = data;
+    this.type = type;
+  }
+
+  public void setData(Data data) { this.data = data; }
+  public void setDirection(Integer direction) { this.direction = direction; }
+  public void setType(Action type) { this.type = type; }
+
+  public Data getData() { return this.data; }
+  public Integer getDirection() { return this.direction; }
+  public Action getType() { return this.type; }
+
+  public Integer getDx() {
+    Integer dx = 0;
+    switch this.direction {
+      case 1: dx = 1; break;
+      case 2: dx = 0; break;
+      case 3: dx = -1; break;
+      case 4: dx = 0; break;
+      default: dx = 0; break;
+    }
+    return dx;
+  }
+
+  public Integer getDy() {
+    Integer dy = 0;
+    switch this.direction {
+      case 1: dy = 0; break;
+      case 2: dy = 1; break;
+      case 3: dy = 0; break;
+      case 4: dy = -1; break;
+      default: dy = 0; break;
+    }
+    return dy;
+  }
+
+  private String data;
+  private Integer direction;
+  private Action type;
+}
